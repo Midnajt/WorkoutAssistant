fe51749ffc6a8b1cad394b36e424bdbc99c7ff6f
Marcin_Cer <midnajt0@gmail.com>
2023-04-05 14:50:46 +0200

    sprawdz czy dziala na android

--- app.js
+++ app.js
@@ -1,38 +1,25 @@
 const btn = document.querySelector("#btnStart");
-const texts = ["jesteś dzikiem", "do it bejbi, do it", "Nakurwiaj Michaś", "You can do it", "naaaah, nahhhh, nahhh, i na cyce grubasowi", "no dalej, zrób to jeszcze raz, postaraj się dla mnie", "nie przestawaj, już prawie"];
-// function speak(text) {
-//   if ("speechSynthesis" in window) {
-//     var msg = new SpeechSynthesisUtterance();
-//     msg.text = text;
-//     window.speechSynthesis.speak(msg);
-//   } else {
-//     console.error("Speech synthesis not supported.");
-//   }
-// }
+const texts = ["jesteś dzikiem", "du it beeejjbi, du it", "Nakurwiaj Michaś", "You ken du it", "naaaah, nahhhh, nahhh, i na cyce grubasowi", "no dalej, zrób to jeszcze raz, postaraj się dla mnie", "nie przestawaj, już prawie"];
 
 btn.addEventListener("click", () => {
-  const randomNumber = Math.floor(Math.random() * 6);
+  const randomNumber = Math.floor(Math.random() * texts.length - 1);
+  console.log(randomNumber);
   speak(texts[randomNumber]);
 });
 
 function speak(text) {
-  // Check if Web Speech API is supported
-  if ("speechSynthesis" in window) {
-    var msg = new SpeechSynthesisUtterance();
-    msg.text = text;
-
-    // Choose a voice that supports the language you want to speak
-    var voices = window.speechSynthesis.getVoices();
+  var synth = window.speechSynthesis;
+  if (synth) {
+    var utterance = new SpeechSynthesisUtterance(text);
+    var voices = synth.getVoices();
     for (var i = 0; i < voices.length; i++) {
-      if (voices[i].lang === "en-US" || voices[i].lang === "en_GB" || voices[i].lang === "pl-PL") {
-        msg.voice = voices[i];
+      if (voices[i].lang === "pl-PL") {
+        utterance.voice = voices[i];
         break;
       }
     }
-
-    // Speak the message
-    window.speechSynthesis.speak(msg);
+    synth.speak(utterance);
   } else {
-    alert("Brak wsparcia dla syntezatora mowy, gadaj se sam!");
+    alert("Nic nie powiem!");
   }
 }
