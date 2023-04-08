83d5fee5ac8768f3ce1522f81e143f2cdb99fea5
Midnajt <midnajt0@gmail.com>
2023-04-08 09:28:57 +0200

    progress

--- js/App.js
+++ js/App.js
@@ -6,11 +6,37 @@ class App {
   initializeApp() {
     // console.log("hello js");
     // this.#SpeechSynthesizer.init();
-    this.chaos();
+    this.addChaosToBtn();
+  }
+
+  addChaosToBtn() {
+    const btn = document.querySelector("#btnStart");
+
+    btn.addEventListener("click", this.chaos);
   }
 
   chaos() {
-    console.log("chaos");
+    const timerSpan = document.querySelector("#timer");
+    const time = document.querySelector("[data-exercise='pompki']").value;
+    let currentTime = 1;
+
+    console.log({ currentTime });
+    const interval = setInterval(() => {
+      if (currentTime == time / 2) {
+        console.log("połowa");
+      }
+      console.log("interval");
+      timerSpan.innerHTML = currentTime;
+      if (currentTime == time) {
+        clearInterval(interval);
+        timerSpan.innerHTML = "Koniec";
+      }
+      currentTime++;
+    }, 1000);
+
+    // setTimeout(() => {
+    //   clearInterval(interval);
+    // }, 3000);
   }
 }
 
